a720189a101701dfa849b7237830f86e3ce6a78d
yaya847 <139044382+yaya847@users.noreply.github.com>
2025-06-01 19:22:22 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -92,4 +92,5 @@ app.get('/logout', (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log('Serveur en cours sur http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Serveur en cours sur le port ${PORT}`));
